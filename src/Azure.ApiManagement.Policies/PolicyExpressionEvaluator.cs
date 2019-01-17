48a16609c240edb90f61b682200c5bdd2c9f5a28
Kevin Hillinger <kevin.hillinger@live.com>
2019-01-17 13:31:00 -0500

    adding policy expression evaluator

--- src/Azure.ApiManagement.Policies/PolicyExpressionEvaluator.cs
+++ src/Azure.ApiManagement.Policies/PolicyExpressionEvaluator.cs
@@ -1,17 +1,35 @@
 ﻿using Azure.ApiManagement.Policies.Variables;
+using NSubstitute;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Azure.ApiManagement.Policies
 {
+    /// <summary>
+    /// Expression evaluator
+    /// </summary>
     public class PolicyExpressionEvaluator
     {
-        public virtual T Evaluate<T>(IContext context)
+        private IContext context;
+
+        public PolicyExpressionEvaluator(IContext context)
         {
-            return default(T);
+            this.context = context;
+        }
+
+        public PolicyExpressionEvaluator() : this(Substitute.For<IContext>())
+        {
+
+        }
+
+        public PolicyExpressionEvaluator Using(IContext context)
+        {
+            this.context = context;
+            return this;
+        }
+
+        public virtual T Evaluate<T>(Func<IContext, T> expression)
+        {
+            return expression(context);
         }
     }
 }
